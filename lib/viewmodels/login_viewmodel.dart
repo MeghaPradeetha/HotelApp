e9545d9072bb30cef46db0735fb55423d9a9e188
Megha Pradeetha <116329674+chaninthamegha@users.noreply.github.com>
2024-06-20 12:58:50 +0530

    update user data

--- lib/viewmodels/login_viewmodel.dart
+++ lib/viewmodels/login_viewmodel.dart
@@ -1,36 +1,49 @@
 import 'dart:developer';
-
 import 'package:flutter/material.dart';
 import 'package:google_sign_in/google_sign_in.dart';
 import 'package:hotel/views/hotel_list_view.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class LoginViewModel extends ChangeNotifier {
   final GoogleSignIn _googleSignIn = GoogleSignIn();
 
+  String? userEmail;
+  String? userName;
+  String? userPhotoUrl;
+
   bool get isLoggedIn => _googleSignIn.currentUser != null;
 
-  //google sign function
-  Future<void> googleSignIn() async {
+  Future<void> googleSignIn(BuildContext context) async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
     try {
       await _googleSignIn.signIn();
-      notifyListeners();
+      if (_googleSignIn.currentUser != null) {
+        userEmail = _googleSignIn.currentUser!.email;
+        userName = _googleSignIn.currentUser!.displayName;
+        userPhotoUrl = _googleSignIn.currentUser!.photoUrl;
+
+        await prefs.setString('userEmail', userEmail!);
+        await prefs.setString('userName', userName!);
+        await prefs.setString('userPhotoUrl', userPhotoUrl!);
+
+        notifyListeners();
+      }
     } catch (error) {
       log('Google sign in failed: $error');
     }
   }
 
-  //navigator for Hotel List View
   Future<void> navigateToHotelListView(BuildContext context) async {
-    await googleSignIn();
-    Future.delayed(Duration.zero, () {
-      if (isLoggedIn) {
-        Navigator.push(
-          context,
-          MaterialPageRoute(
-            builder: (context) => const HotelListView(),
-          ),
-        );
-      }
-    });
+    await googleSignIn(context);
+    if (isLoggedIn) {
+      Navigator.push(
+        context,
+        MaterialPageRoute(
+          builder: (context) => const HotelListView(),
+        ),
+      );
+    } else {
+      log('User is not logged in.');
+    }
   }
 }
--- lib/views/hotel_list_view.dart
+++ lib/views/hotel_list_view.dart
@@ -1,10 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:hotel/viewmodels/login_viewmodel.dart';
 import 'package:provider/provider.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 import '../viewmodels/hotel_list_viewmodel.dart';
 import '../widgets/hotel_card.dart';
 
 class HotelListView extends StatefulWidget {
-  const HotelListView({super.key});
+  const HotelListView({Key? key}) : super(key: key);
 
   @override
   State<HotelListView> createState() => _HotelListViewState();
@@ -12,53 +14,64 @@ class HotelListView extends StatefulWidget {
 
 class _HotelListViewState extends State<HotelListView> {
   final HotelListViewModel model = HotelListViewModel();
-
-  SizedBox sizeBox(double size) => SizedBox(height: size);
+  final LoginViewModel logModel = LoginViewModel();
+  String uName = "";
+  String email = "";
 
   @override
   void initState() {
     super.initState();
     model.fetchAllHotels();
+    getUserData();
+  }
+
+  Future<void> getUserData() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    setState(() {
+      uName = prefs.getString('userName') ?? '';
+      email = prefs.getString('userEmail') ?? '';
+    });
   }
 
   Widget hotelsList() => Container(
-      height: MediaQuery.of(context).size.height / 1.1,
-      padding: const EdgeInsets.only(left: 15.0, right: 15.0, top: 120.0),
-      child: model.isLoading
-          ? const Center(
-              child: CircularProgressIndicator(),
-            )
-          : model.filteredHotels.isNotEmpty
-              ? Scrollbar(
-                  thickness: 9,
-                  interactive: true,
-                  thumbVisibility: true,
-                  radius: const Radius.circular(4),
-                  child: Padding(
-                    padding: const EdgeInsets.all(20.0),
-                    child: ListView.separated(
-                      scrollDirection: Axis.vertical,
-                      shrinkWrap: true,
-                      itemCount: model.filteredHotels.length,
-                      itemBuilder: (context, index) {
-                        return HotelCard(hotel: model.filteredHotels[index]);
-                      },
-                      separatorBuilder: (context, index) {
-                        return const Divider(
-                          indent: 5,
-                          endIndent: 5,
-                          color: Colors.grey,
-                        );
-                      },
+        height: MediaQuery.of(context).size.height / 1.1,
+        padding: const EdgeInsets.only(left: 15.0, right: 15.0, top: 120.0),
+        child: model.isLoading
+            ? const Center(
+                child: CircularProgressIndicator(),
+              )
+            : model.filteredHotels.isNotEmpty
+                ? Scrollbar(
+                    thickness: 9,
+                    interactive: true,
+                    thumbVisibility: true,
+                    radius: const Radius.circular(4),
+                    child: Padding(
+                      padding: const EdgeInsets.all(20.0),
+                      child: ListView.separated(
+                        scrollDirection: Axis.vertical,
+                        shrinkWrap: true,
+                        itemCount: model.filteredHotels.length,
+                        itemBuilder: (context, index) {
+                          return HotelCard(hotel: model.filteredHotels[index]);
+                        },
+                        separatorBuilder: (context, index) {
+                          return const Divider(
+                            indent: 5,
+                            endIndent: 5,
+                            color: Colors.grey,
+                          );
+                        },
+                      ),
+                    ),
+                  )
+                : const Center(
+                    child: Text(
+                      'No hotels available.',
+                      style: TextStyle(fontSize: 18, color: Colors.black),
                     ),
                   ),
-                )
-              : const Center(
-                  child: Text(
-                    'No hotels available.',
-                    style: TextStyle(fontSize: 18, color: Colors.black),
-                  ),
-                ));
+      );
 
   Widget mainBody(BuildContext context) => Stack(
         children: [
@@ -72,15 +85,31 @@ class _HotelListViewState extends State<HotelListView> {
                 Row(
                   mainAxisAlignment: MainAxisAlignment.spaceBetween,
                   children: [
-                    const Text(
-                      'Hotels List',
-                      style: TextStyle(
-                        fontSize: 24.0,
-                        fontWeight: FontWeight.bold,
-                        color: Colors.blueAccent,
-                      ),
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          uName,
+                          style: const TextStyle(
+                            fontSize: 24.0,
+                            fontWeight: FontWeight.bold,
+                            color: Colors.blueAccent,
+                          ),
+                        ),
+                        Text(
+                          email,
+                          style: const TextStyle(
+                            fontSize: 13.0,
+                            fontWeight: FontWeight.bold,
+                            color: Colors.blueGrey,
+                          ),
+                        ),
+                      ],
                     ),
                     ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: Colors.blueGrey,
+                      ),
                       onPressed: () async {
                         await model.signOut(context);
                       },
@@ -88,7 +117,7 @@ class _HotelListViewState extends State<HotelListView> {
                     ),
                   ],
                 ),
-                sizeBox(10.0),
+                const SizedBox(height: 10.0),
                 TextField(
                   decoration: InputDecoration(
                     hintText: 'Search hotels...',
